1079b4efc06c693d49b52b5b6a753555edc8908d
Pedro Corado <pedrocorado2025@gmail.com>
2023-11-13 01:14:30 -0300

    Atualizando codigo - forçando a adição de um '.'  apos o primeiro numero

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,22 @@ import React, { useState } from 'react';
 import './components/MeuComponente.css';
 
 function App() {
-  const [altura , setAltura] = useState('')
-  const [peso, setPeso] = useState('')
-  
+  const [altura, setAltura] = useState('');
+  const [peso, setPeso] = useState('');
+
   const alturaNumero = parseFloat(altura);
   const pesoNumero = parseFloat(peso);
 
   const calculo = pesoNumero / (alturaNumero * alturaNumero);
 
-
   const handleAlturaChange = (event) => {
-    setAltura(event.target.value);
+    const novaAltura = event.target.value;
+
+    if (/^\d$/.test(novaAltura)) {
+      setAltura(novaAltura + '.');
+    } else {
+      setAltura(novaAltura);
+    }
   };
 
   const handlePesoChange = (event) => {
@@ -21,15 +26,25 @@ function App() {
 
   return (
     <div>
-        <div className="header">
-          <h1>Tabela de IMC</h1>
-        </div>
-      <main className='main'>
-          <input className='mainItens' value={altura} type="number" onChange={handleAlturaChange} placeholder='Digite sua Altura' />
-          <input className='mainItens' value={peso} type="number" onChange={handlePesoChange} placeholder='Digite seu Peso' />
-          <p>
-            {calculo.toFixed(2)}
-          </p>
+      <div className="header">
+        <h1>Tabela de IMC</h1>
+      </div>
+      <main className="main">
+        <input
+          className="mainItens"
+          value={altura}
+          type="text"
+          onChange={handleAlturaChange}
+          placeholder="Digite sua Altura"
+        />
+        <input
+          className="mainItens"
+          value={peso}
+          type="number"
+          onChange={handlePesoChange}
+          placeholder="Digite seu Peso"
+        />
+        <p>{isNaN(calculo) ? 'Digite altura e peso válidos' : calculo.toFixed(2)}</p>
       </main>
     </div>
   );
